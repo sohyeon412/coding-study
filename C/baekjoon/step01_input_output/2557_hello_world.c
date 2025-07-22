fbd6c0e42f85339c38ee1a109cc0e61c92936e94
Soso6539 <sohyeon.ac@gmail.com>
2025-07-22 12:59:34 +0900

    백준 2557번 Hello World 출력 (java)

--- C/baekjoon/step01_input_output/2557_hello_world.c
+++ C/baekjoon/step01_input_output/2557_hello_world.c
@@ -1,8 +1,3 @@
-//문제: https://www.acmicpc.net/problem/2557
-//제목: Hello World
-//단계: 입력과 출력
-//설명: Hello World!출력.
-
 #include<stdio.h>               //입출력 함수 사용 헤더 파일
 
 int main(void){                 //main함수 : c프로그램 시작점
---
+++ python/baekjoon/step01_input_output/2557_hello_world.py
@@ -0,0 +1,4 @@
+#문제: https://www.acmicpc.net/problem/2557
+#제목: Hello World
+#단계: 입력과 출력
+#설명: Hello World!출력.
