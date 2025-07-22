d5bf5d2d3941b4fdbfeb1628d1757cbefe9ce493
Soso6539 <sohyeon.ac@gmail.com>
2025-07-22 15:57:15 +0900

    백준 1008번 연산문제(/)(java)

---
+++ java/baekjoon/step01_input_output/MainDiv.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainDiv{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double A = sc.nextInt();
+    double B = sc.nextInt();
+    System.out.printf("%.9f\n", A/B);
+    }
+}
