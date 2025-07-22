635259dd8f6d67be0e4585a53002388ae191c764
Soso6539 <sohyeon.ac@gmail.com>
2025-07-22 14:59:05 +0900

    백준 1000,1001번 Hello World 출력 (c,java,python)

---
+++ C/baekjoon/step01_input_output/1000_A_plus_B.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+    int A, B;
+    scanf("%d %d", &A, &B);  //입력
+    printf("%d", A+B);       //출력
+    return 0;
+}
--- python/baekjoon/step01_input_output/2557_hello_world.py
+++ python/baekjoon/step01_input_output/2557_hello_world.py
@@ -1,4 +1,1 @@
-#문제: https://www.acmicpc.net/problem/2557
-#제목: Hello World
-#단계: 입력과 출력
-#설명: Hello World!출력.
+print("Hello World!")
