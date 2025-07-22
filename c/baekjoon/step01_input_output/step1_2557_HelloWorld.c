a9d547e00e806adfd621b8aa70a91214ef68a7e1
Soso6539 <sohyeon.ac@gmail.com>
2025-07-22 10:45:51 +0900

    백준 2557번 Hello World 출력 (C)

---
+++ c/baekjoon/step01_input_output/step1_2557_HelloWorld.c
@@ -0,0 +1,11 @@
+//문제: https://www.acmicpc.net/problem/2557
+//제목: Hello World
+//단계: 입력과 출력
+//설명: Hello World!출력.
+
+#include<stdio.h>               //입출력 함수 사용 헤더 파일
+
+int main(void){                 //main함수 : c프로그램 시작점
+    printf("Hello World!");     //출력 명령어 : printf, 출력할 내용: "따옴표 안에 쓰기"
+    return 0;                   // 함수 끝
+}
