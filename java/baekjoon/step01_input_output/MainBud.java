60d3198716632a34348b95e1fa6671077e010bad
Soso6539 <sohyeon.ac@gmail.com>
2025-07-23 12:24:40 +0900

    백준 18108번 서기연도 문제(java)

---
+++ java/baekjoon/step01_input_output/MainBud.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MainBud{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int BE = sc.nextInt();
+        System.out.println(BE - 543);
+    }
+}
