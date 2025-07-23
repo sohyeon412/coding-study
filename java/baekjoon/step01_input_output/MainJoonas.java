6d4436c003b555cb885ce2d6cbace5f5535e2e88
Soso6539 <sohyeon.ac@gmail.com>
2025-07-23 11:26:09 +0900

    백준 10926번 준하문제(java)

--- java/baekjoon/step01_input_output/MainJoonas.java
+++ java/baekjoon/step01_input_output/MainJoonas.java
@@ -1,3 +1,9 @@
 import java.util.Scanner;
 
-public class MainJoonas
+public class MainJoonas{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name + "??!");
+    }
+}
