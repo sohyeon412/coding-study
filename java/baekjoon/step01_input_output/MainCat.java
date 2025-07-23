81f88923d95e4a770adecfe992a3f1b0bee2e1e7
Soso6539 <sohyeon.ac@gmail.com>
2025-07-23 16:08:21 +0900

    백준 10171번 고양이 문제(c,java)

---
+++ java/baekjoon/step01_input_output/MainCat.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainCat{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
